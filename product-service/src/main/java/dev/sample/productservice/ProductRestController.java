a3008f9f6af69003bac110f998692dbc7abcfa9a
Vuthy_Tourn <vuthytuon168@gmail.com>
2026-05-19 22:27:24 +0700

    First Commit

---
+++ product-service/src/main/java/dev/sample/productservice/ProductRestController.java
@@ -0,0 +1,24 @@
+package dev.sample.productservice;
+
+
+import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+@Hidden
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductRestController {
+
+    @GetMapping("/get-all")
+    public ResponseEntity<?> getAllProducts(){
+        var products = new ArrayList<ProductResponse>();
+        products.add(new ProductResponse(UUID.randomUUID(), "Product 1", "Description 1", 45));
+        return ResponseEntity.ok(products);
+    }
+}
